13b1b4f40af5940ff89e03ab9594e53d96a4984c
JeisonYamarte <jeisonyamarte16@gmail.com>
2025-09-19 23:28:24 -0300

    section impulse complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import { CardLandingPage } from "@/components/cardLandingPage";
 import { Button } from "@/components/ui/button"
-import { MoveRight, FileText, ShieldCheck, Cloud, Palette} from 'lucide-react';
+import { MoveRight, FileText, ShieldCheck, Cloud, Palette, UsersRound, Laptop, } from 'lucide-react';
 import { Inter } from "next/font/google";
-import Image from "next/image";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,7 +25,7 @@ export default async function Home() {
           </div>
         </div>
       </header>
-      <section className="w-full max-w-[1140px] h-96 mt-10 p-5">
+      <section className="w-full max-w-[1140px] mt-10 p-5 h-auto">
         <div className="text-center mb-10">
           <h2 className="text-3xl font-bold mb-10">Características que marcan la diferencia</h2>
           <div className="grid grid-cols-3 grid-rows-2 gap-4">
@@ -33,6 +33,17 @@ export default async function Home() {
             <CardLandingPage icon={<ShieldCheck />} title="Seguridad de Datos Avanzada" description="Proteja la información confidencial del paciente con cifrado de grado industrial y protocolos de privacidad estrictos. Cumpla con las normativas de salud sin esfuerzo." />
             <CardLandingPage icon={<Cloud />} title="Acceso Seguro en la Nube" description="Acceda a sus notas desde cualquier lugar y en cualquier momento con nuestra plataforma basada en la nube. Realice copias de seguridad automáticas y mantenga sus datos sincronizados de forma segura." />
             <CardLandingPage icon={<Palette />} title="Interfaz de Usuario Adaptable" description="Disfrute de una experiencia fluida y consistente en cualquier dispositivo. Nuestra interfaz está diseñada para profesionales de la salud, optimizada para la eficiencia." />
+          </div>
+        </div>
+      </section>
+            <section className="w-full max-w-[1140px] h-96 mt-10 p-5">
+        <div className="text-center mb-10">
+          <h2 className="text-3xl font-bold mb-10">Impulse su practica medica</h2>
+          <div className="grid grid-cols-3 grid-rows-2 gap-4">
+            <CardLandingPage icon={<UsersRound />} title="Colaboración Eficiente" description="Comparta notas de forma segura con colegas y equipos médicos, mejorando la coordinación de la atención al paciente y minimizando errores."/>
+            <CardLandingPage icon={<Laptop />} title="Integración de Sistemas" description="Conecte 'Notas Médicas' con sus sistemas existentes de gestión de pacientes (EMR) para un flujo de trabajo unificado y sin interrupciones." />
+            <CardLandingPage icon={<FileText />} title="Plantillas Personalizables" description="Cree y use plantillas personalizadas para diferentes tipos de notas clínicas, ahorrando tiempo y asegurando la exhaustividad en la documentación." />
+            <CardLandingPage icon={<ShieldCheck />} title="Auditoría y Conformidad" description="Mantenga un registro completo de todas las modificaciones y accesos a las notas, facilitando la auditoría y demostrando el cumplimiento normativo." />
           </div>
         </div>
       </section>
