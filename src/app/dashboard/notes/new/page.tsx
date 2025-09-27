68e12145ffdc078a4713e06f5809f28917fb7836
JeisonYamarte <jeisonyamarte16@gmail.com>
2025-09-27 00:02:55 -0300

    new note: style complete

--- src/app/dashboard/notes/new/page.tsx
+++ src/app/dashboard/notes/new/page.tsx
@@ -1,13 +1,14 @@
 "use client"
+import React, { useCallback} from 'react'
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {useDropzone} from 'react-dropzone'
 
 import { Button } from "@/components/ui/button"
 import {
     Form,
     FormControl,
-    FormDescription,
     FormField,
     FormItem,
     FormLabel,
@@ -16,7 +17,6 @@ import {
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { Label } from "@/components/ui/label"
 
 
 
@@ -60,51 +60,27 @@ export default function NewNotePage() {
         console.log('form:',data);
     };
 
+
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        // Do something with the files
+    }, [])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
     return (
         <div>
             <h2 className="text-xl font-bold mb-4">Crear nueva nota</h2>
-            <div>
-                <Form {...form}>
-                    <form id="new-note-form" onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-                        <div>
-                            <FormField
-                            control={form.control}
-                            name="title"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Título</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Título de la nota" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                            />
-                            <FormField
-                            control={form.control}
-                            name="content"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Contenido</FormLabel>
-                                    <FormControl>
-                                        <Textarea className="w-full h-[400px]" placeholder="Contenido de la nota" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                            />
-                        </div>
-                        <div>
-                            <h2>Información</h2>
-                            <div className="grid grid-cols-2 grid-row-2 gap-4">
+            <div className="flex gap-5">
+                <div className="w-2/3">
+                    <Form {...form}>
+                        <form id="new-note-form" onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                            <div className=" flex flex-col gap-5">
                                 <FormField
                                 control={form.control}
-                                name="patient"
+                                name="title"
                                 render={({ field }) => (
                                     <FormItem>
-                                        <FormLabel>Paciente</FormLabel>
                                         <FormControl>
-                                            <Input placeholder="Nombre del paciente" {...field} />
+                                            <Input placeholder="Título de la nota" {...field} />
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
@@ -112,66 +88,107 @@ export default function NewNotePage() {
                                 />
                                 <FormField
                                 control={form.control}
-                                name="noteType"
+                                name="content"
                                 render={({ field }) => (
                                     <FormItem>
-                                        <FormLabel>Tipo de nota</FormLabel>
-                                            <FormControl>
-                                                <Select onValueChange={field.onChange} defaultValue={field.value}>
-                                                        <SelectTrigger className="w-full h-10 bg-white">
-                                                            <SelectValue placeholder="selecciona el tipo de nota" />
-                                                        </SelectTrigger>
-                                                        <SelectContent>
-                                                            <SelectItem value={NoteType.FOLLOWUP}>{NoteType.FOLLOWUP}</SelectItem>
-                                                            <SelectItem value={NoteType.CONSULTATION}>{NoteType.CONSULTATION}</SelectItem>
-                                                            <SelectItem value={NoteType.EMERGENCY}>{NoteType.EMERGENCY}</SelectItem>
-                                                        </SelectContent>
-                                                </Select>
-                                            </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                                />
-                                <FormField
-                                control={form.control}
-                                name="urgencyLevel"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Nivel de urgencia</FormLabel>
-                                            <FormControl>
-                                                <Select onValueChange={field.onChange} defaultValue={field.value}>
-                                                        <SelectTrigger className="w-full h-10 bg-white">
-                                                            <SelectValue placeholder="selecciona el nivel de urgencia" />
-                                                        </SelectTrigger>
-                                                        <SelectContent>
-                                                            <SelectItem value={UrgencyLevel.LOW}>{UrgencyLevel.LOW}</SelectItem>
-                                                            <SelectItem value={UrgencyLevel.MEDIUM}>{UrgencyLevel.MEDIUM}</SelectItem>
-                                                            <SelectItem value={UrgencyLevel.HIGH}>{UrgencyLevel.HIGH}</SelectItem>
-                                                        </SelectContent>
-                                                </Select>
-                                            </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                                />
-                                <FormField
-                                control={form.control}
-                                name="tags"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Etiquetas</FormLabel>
                                         <FormControl>
-                                            <Input placeholder="Escribe las etiquetas de la nota" {...field} />
+                                            <Textarea className="resize-none w-full h-[400px]" placeholder="Contenido de la nota" {...field} />
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
                                 )}
                                 />
                             </div>
-                        </div>
-                        <Button type="submit">Guardar nota</Button>                                
-                    </form>
-                </Form>
+                            <div>
+                                <h2>Información</h2>
+                                <div className="grid grid-cols-2 grid-row-2 gap-4">
+                                    <FormField
+                                    control={form.control}
+                                    name="patient"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Paciente</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="Nombre del paciente" {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                    />
+                                    <FormField
+                                    control={form.control}
+                                    name="noteType"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Tipo de nota</FormLabel>
+                                                <FormControl>
+                                                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                                            <SelectTrigger className="w-full h-10 bg-white">
+                                                                <SelectValue placeholder="selecciona el tipo de nota" />
+                                                            </SelectTrigger>
+                                                            <SelectContent>
+                                                                <SelectItem value={NoteType.FOLLOWUP}>{NoteType.FOLLOWUP}</SelectItem>
+                                                                <SelectItem value={NoteType.CONSULTATION}>{NoteType.CONSULTATION}</SelectItem>
+                                                                <SelectItem value={NoteType.EMERGENCY}>{NoteType.EMERGENCY}</SelectItem>
+                                                            </SelectContent>
+                                                    </Select>
+                                                </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                    />
+                                    <FormField
+                                    control={form.control}
+                                    name="urgencyLevel"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Nivel de urgencia</FormLabel>
+                                                <FormControl>
+                                                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                                            <SelectTrigger className="w-full h-10 bg-white">
+                                                                <SelectValue placeholder="selecciona el nivel de urgencia" />
+                                                            </SelectTrigger>
+                                                            <SelectContent>
+                                                                <SelectItem value={UrgencyLevel.LOW}>{UrgencyLevel.LOW}</SelectItem>
+                                                                <SelectItem value={UrgencyLevel.MEDIUM}>{UrgencyLevel.MEDIUM}</SelectItem>
+                                                                <SelectItem value={UrgencyLevel.HIGH}>{UrgencyLevel.HIGH}</SelectItem>
+                                                            </SelectContent>
+                                                    </Select>
+                                                </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                    />
+                                    <FormField
+                                    control={form.control}
+                                    name="tags"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Etiquetas</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="Escribe las etiquetas de la nota" {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                    />
+                                </div>
+                            </div>
+                            <Button type="submit">Guardar nota</Button>                                
+                        </form>
+                    </Form>
+                </div>
+                <div className="w-1/3 flex gap-3 flex-col border-1 border-gray-300 p-3 rounded-lg max-w-[400px] justify-center items-center">
+                    <h2 className="text-lg font-semibold">Contextos o referencias</h2>
+                        <div className='h-full w-full border-3 border-dashed border-gray-300 max-w-[300px] rounded-lg text-center items-center' {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        {
+                            isDragActive ?
+                            <p>Drop the files here ...</p> :
+                            <p>Drag 'n' drop some files here, or click to select files</p>
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     );
